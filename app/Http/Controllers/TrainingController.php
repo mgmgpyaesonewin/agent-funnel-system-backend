8fcac23dc9e87996807951a086c4489f5304dd6b
pkk2332 <babyking67@gmail.com>
2020-07-21 23:50:11 +0630

    training module finished

---
+++ app/Http/Controllers/TrainingController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TrainingRequest;
+use App\Training;
+use Illuminate\Http\Request;
+
+class TrainingController extends Controller
+{
+
+    public function index()
+    {
+        $trainings = Training::all();
+        return view('pages.training.index',compact('trainings'));
+    }
+
+    public function create()
+    {
+        return view('pages.training.create');
+
+    }
+
+  
+    public function store(TrainingRequest $request)
+    {
+       
+        Training::create($request->validated());
+        return view('pages.training.index');
+
+    }
+
+ 
+    public function show(Training $training)
+    {
+
+    }
+
+  
+    public function edit(Training $training)
+    {
+        
+        return view('pages.training.edit',compact('training'));
+
+    }
+
+    public function update(TrainingRequest $request, Training $training)
+    {
+        $training->update(
+            $request->validated()
+        );
+      return redirect('/trainings');
+    }
+
+
+    public function destroy(Training $training)
+    {
+        // dd($training);
+        $training->delete();
+        return redirect('/trainings');
+    }
+}
