9f556d26f3a92d6545e02d3546eb77da4dee903f
Pyae Sone <mgmgpyaesonewin@gmail.com>
2020-08-20 12:18:22 +0630

    wip

--- resources/views/pages/applicants/pmli_filter.blade.php
+++ resources/views/pages/applicants/pmli_filter.blade.php
@@ -22,9 +22,9 @@
             <div class="card">
                 <div class="card-content">
                     <div class="card-body text-center">
-                        <v-table ref="table" current-status="pmli_filter" :status="[1,2,3,5,6]" :user-assign="true"
-                            :assign-checkbox="true" assign="true" channel="true" status-col="true" temp-id="true"
-                            status-col="true" :is-partner="{{ auth()->user()->partner_id != null ? 1 : 0 }}">>
+                        <v-table ref="table" current-status="pmli_filter" :status="[1,2,3,5,6]" :assign-checkbox="true"
+                            channel="true" status-col="true" temp-id="true" :aml-status="true" status-col="true"
+                            :is-partner="{{ auth()->user()->partner_id != null ? 1 : 0 }}">>
                             <template scope="{ applicant }">
                                 <td>
                                     <div class="btn-group mt-1" v-show="applicant.status_id === 1">
--- resources/views/pages/applicants/pre_filter.blade.php
+++ resources/views/pages/applicants/pre_filter.blade.php
@@ -23,8 +23,8 @@
                 <div class="card-content">
                     <div class="card-body text-center">
                         <v-table ref="table" current-status="pre_filter" :status="[1,7]" channel="true"
-                            :assign-checkbox="true" status-col="true" :aml-status="true" status-col="true"
-                            :is-partner="{{ auth()->user()->partner_id != null ? 1 : 0 }}">
+                            :assign-checkbox="true" status-col="true" status-col="true" :user-assign="true"
+                            assign="true" :is-partner="{{ auth()->user()->partner_id != null ? 1 : 0 }}">
                             <template scope="{ applicant }">
                                 <div class="btn-group mt-1"
                                     v-show="applicant.status_id === 1 && applicant.aml_status == 'Agreed'">
