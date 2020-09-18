bceb4468765740c8f7b73e15180902e76596346a
Pyae Sone <mgmgpyaesonewin@gmail.com>
2020-09-18 14:01:24 +0630

    bug fixed

--- app/Http/Controllers/TemplateFormController.php
+++ app/Http/Controllers/TemplateFormController.php
@@ -59,7 +59,7 @@ class TemplateFormController extends Controller
         $templates = TemplateForm::where('active', true)->first();
 
         $applicant = Applicant::where('uuid', $req->id)->where('current_status', 'pre_filter')->where('status_id', 1)->first();
-        $term_condition = Setting::where('meta_key', "document_tnc{$req->lang}")->first()->meta_value;
+        $term_condition = Setting::where('meta_key', "document_tnc_{$req->lang}")->first()->meta_value;
 
         if ($applicant) {
             return [
