f6733b82975e0d5e65703397ff5abd7fe2eb470d
Pyae Sone <mgmgpyaesonewin@gmail.com>
2020-11-27 21:26:03 +0630

    Remove bot

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -43,8 +43,6 @@ class Kernel extends ConsoleKernel
             $schedule->job(new GenerateRelatedPersonEntity())->dailyAt($time); // Related person
         }
         $schedule->command('fake:applicant')->dailyAt('7:55');
-        $schedule->command('fake:applicant')->dailyAt('11:55');
-        $schedule->command('fake:applicant')->dailyAt('15:55');
     }
 
     /**
