27acf1dca25708f44a3f6bae0fb266f802a8f150
Pyae Sone <mgmgpyaesonewin@gmail.com>
2021-01-29 13:43:01 +0630

    Schedule log

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,6 +12,7 @@ use App\Jobs\GenerateProducerEntity;
 use App\Jobs\GenerateRelatedPersonEntity;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -32,15 +33,32 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        foreach (['1:00'] as $time) {
+        foreach (['14:00', '15:00', '2:00', '3:00'] as $time) {
+            Log::info('Running File Export');
+
             $schedule->job(new GenerateContactEntity())->dailyAt($time); // CONT
+            Log::info('Running File Export GenerateContactEntity');
+
             $schedule->job(new GenerateContractEntity())->dailyAt($time); // CONTR
+            Log::info('Running File Export GenerateContractEntity');
+
             $schedule->job(new GenerateEducationEntity())->dailyAt($time); // EDU
+            Log::info('Running File Export GenerateEducationEntity');
+
             $schedule->job(new GenerateLicenseEntity())->dailyAt($time); // LICN
+            Log::info('Running File Export GenerateLicenseEntity');
+
             $schedule->job(new GeneratePayeeBankEntity())->dailyAt($time); // BANK
+            Log::info('Running File Export GeneratePayeeBankEntity');
+
             $schedule->job(new GenerateProducerAdditionalInformationEntity())->dailyAt($time); // ADD_INFO
+            Log::info('Running File Export GenerateProducerAdditionalInformationEntity');
+
             $schedule->job(new GenerateProducerEntity())->dailyAt($time); // PROD
+            Log::info('Running File Export GenerateProducerEntity');
+
             $schedule->job(new GenerateRelatedPersonEntity())->dailyAt($time); // Related person
+            Log::info('Running File Export GenerateRelatedPersonEntity');
         }
 //        $schedule->command('fake:applicant')->dailyAt('7:55');
     }
